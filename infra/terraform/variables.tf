b3e86ac7b6dd449b200a11fdcd3b9a658f3104e4
Gabriel Melo <melogoncalvesbiel@gmail.com>
2023-08-16 16:52:35 -0300

    fix: variables

--- infra/terraform/variables.tf
+++ infra/terraform/variables.tf
@@ -1,7 +1,6 @@
 variable "do_access_token" {
   description = "Token de acesso a API do Digital Ocean"
 }
-variable "ssh_keys" {
-  default = ""
-  description = "SSH key"
+variable "my_ssh_key" {
+  description = "Your ssh key"
 }
--- infra/terraform/vm.tf
+++ infra/terraform/vm.tf
@@ -7,5 +7,5 @@ resource "digitalocean_droplet" "playground" {
 
   tags = ["playground"]
 
-  ssh_keys = ["ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDBX5UNBuOoxSo/7Q7oIoBeAUOX3fTASrOP+sTY6oaDUb3J6B+SeQWlF5SNQzuE/uFPYVzaZ2Sgr/seXOI+eQtdB2CwSSq3SNVTsuAOAp5IcWNLh0fDgR5tMOP0L/EfElMzotr2RbNpsJVkAuC5JT0oEBoZSfVx2g1jnEmaOy3zce+8TXsngNg+Dbo3fiSQ59sJmBZbT1bnW4wPqgfl7XuSmo6/wshm6EISdycuswv6TMUl5pLvzQ2W1AIX32sJNgYKeuDXLhsxxVmu+1ps4OhIfpG2RIWI7eYds4MSE9izWBQDqhFLklAwabMIFn8NJquw70KLF6Uctw8iYADmcveZx7jTNNNX5gmPhScOiBFHvDHxg+aRltPa9s+Mp1J1bvedzsWm0WQuBSYt12rc13Y/+TjbpTT3tDS9nXo9r8iZQRv5/uPN6I0eV9d9rkvG1EjKrinbdlLelhN3aIrvNjQrLcADi89BSzuq77kwH34JzozT5iVjIjw9Fdi1kbEUyyk= gabriel@pop-os"]
+  ssh_keys = [var.my_ssh_key]
 }
